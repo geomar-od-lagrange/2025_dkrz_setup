a19dfffd6963e73db5959437be5365476fe612f0
Willi Rath <wrath@geomar.de>
2025-08-22 16:00:52 +0200

    Fix

--- scripts/setup_parcels_dev_env.sh
+++ scripts/setup_parcels_dev_env.sh
@@ -11,13 +11,13 @@ conda init
 conda shell.bash activate base
 
 # create dev env (still without parcels)
-mamba env create -n parcels-dev -f environment.yml
+mamba env create -n ${ENV_NAME} -f environment.yml
 
 # activate
 conda shell.bash activate ${ENV_NAME}
 
 # add additional relevant packages
-mamba install -c conda-forge python=3 cartopy cftime cmocean copernicusmarine dask datashader distributed geopandas geoviews gsw h5netcdf h5py holoviews hvplot ipykernel matplotlib netcdf4 numcodecs numpy openblas openpyxl pandas papermill pys2index python-blosc rasterio regionmask scikit-learn scipy seaborn seawater sparse xarray xhistogram xoak zarr
+mamba install -y -c conda-forge python=3 cartopy cftime cmocean copernicusmarine dask datashader distributed geopandas geoviews gsw h5netcdf h5py holoviews hvplot ipykernel matplotlib netcdf4 numcodecs numpy openblas openpyxl pandas papermill pys2index python-blosc rasterio regionmask scikit-learn scipy seaborn seawater sparse xarray xhistogram xoak zarr
 
 # install local copy of Parcels code
 python -m pip install -e .  # -e : editable install
